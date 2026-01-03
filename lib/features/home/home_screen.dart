b6d286bb422b035324df3f22e3ebd38f48ccaa55
evan-hwang <id920809@naver.com>
2026-01-03 16:44:34 +0900

    feat: add mandalart list screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:mandalart/features/mandalart/mandalart_store.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final mandalarts = const MandalartStore().loadMandalarts();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mandalart'),
       ),
-      body: const Center(
-        child: Text('Start your Mandalart'),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemCount: mandalarts.length,
+        separatorBuilder: (_, __) => const SizedBox(height: 12),
+        itemBuilder: (context, index) {
+          final mandalart = mandalarts[index];
+
+          return Card(
+            child: ListTile(
+              title: Text(mandalart.title),
+              subtitle: Text(mandalart.dateRangeLabel),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {},
+            ),
+          );
+        },
       ),
     );
   }
---
+++ lib/features/mandalart/mandalart.dart
@@ -0,0 +1,11 @@
+class Mandalart {
+  const Mandalart({
+    required this.id,
+    required this.title,
+    required this.dateRangeLabel,
+  });
+
+  final String id;
+  final String title;
+  final String dateRangeLabel;
+}
